5014a862d1d412c249b3ef3c7e46d785d9974cf8
Jamie <84644341+BasicVariable@users.noreply.github.com>
2023-01-01 17:04:37 -0500

    fixed issue with failed error outputs
    maybe?

--- JS/index.js
+++ JS/index.js
@@ -32,26 +32,14 @@ global.properOutput = {
 };
 
 fs.readFile("./config.yml", 'utf-8', async (err, res) => {
-    if (err) await properOutput.reactiveError(`Failed to read config.yml\nStopping process in 20 seconds...`, 20_000, process.exit());
+    if (err) await properOutput.reactiveError(`Failed to read config.yml\nStopping process in 20 seconds...`, 20_000, process.exit);
     
     try{
         // Gave up trying to make it a var, pretty much ever file needs it
         global.config = yaml.load(res)
     }catch(err){
-        await properOutput.reactiveError(`Failed to read config.yml\nStopping process in 20 seconds...`, 20_000, process.exit());
+        await properOutput.reactiveError(`Failed to read config.yml\nStopping process in 20 seconds...`, 20_000, process.exit);
     };
-
-     // Values 
-    new Promise(async () => {
-        let gettingValues = properOutput.output("Getting values...", "yellow");
-        global.rolimonsValues = await rbxApi.getRolimonsValues();
-        gettingValues("Got values!", "green");
-
-        while (true){
-            await delay(50_000);
-            global.rolimonsValues = await rbxApi.getRolimonsValues();
-        }
-    });
     
     for (cookie of config.auth.cookies){
         if (cookie.length<10) continue;
@@ -70,7 +58,7 @@ fs.readFile("./config.yml", 'utf-8', async (err, res) => {
     };
 
     if (Object.keys(robloxAccounts).length < 1) {
-        await properOutput.reactiveError("All the cookies listed in your config are invalid.\nStopping process in 20 seconds...", 20_000, process.exit())
+        await properOutput.reactiveError("All the cookies listed in your config are invalid.\nStopping process in 20 seconds...", 20_000, process.exit)
     };
 
     // Couldn't think of anything else atm
@@ -78,6 +66,17 @@ fs.readFile("./config.yml", 'utf-8', async (err, res) => {
         Object.values(robloxAccounts).reduce((a, current) => a[a.length+1] = current.name, [])
     }]`.replace(/,/g, ", "), "green");
 
+     // Values 
+    let gettingValues = properOutput.output("Getting values...", "yellow");
+    global.rolimonsValues = await rbxApi.getRolimonsValues();
+    gettingValues("Got values!", "green");
+    new Promise(async () => {
+        while (true){
+            await delay(50_000);
+            global.rolimonsValues = await rbxApi.getRolimonsValues();
+        }
+    });
+    
     // Discord bot
     let startingDiscordBot = properOutput.output("Starting Discord bot...", "yellow");
     await require("./subFiles/discordBot.js").start();;
